9f6abf3f05cf570b4c67f029111257e934d20744
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 16:10:32 -0300

    :sparkles: feat: Implementando componente loading, para informar que a página está sendo carregada

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/loading.css";
+
+function Loading() {
+	return (
+		<>
+			<div className="loading"></div>
+			<h3 className="loading">Loading...</h3>
+		</>
+	);
+}
+
+export default Loading;
