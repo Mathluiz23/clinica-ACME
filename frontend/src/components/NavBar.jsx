0bc0bb095fe10055a860e926342400aa535e749a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 13:50:06 -0300

    :sparkles: feat: Implementando componente de barra de navegação

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { React } from "react";
+
+function NavBar() {
+	return (
+		<nav>
+			<header>Clínica ACME</header>
+			<ul>
+				<a>
+					<li>Pacientes</li>
+				</a>
+			</ul>
+		</nav>
+	);
+}
+
+export default NavBar;
