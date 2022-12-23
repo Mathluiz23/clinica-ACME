53fbb2286e78cfba2bbb15a0098a918100e1dab2
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 18:00:08 -0300

    :sparkles: feat: Implementando lógica de renderização dos pacientes, se houver uma busca no filtro irá renderizar os dados filtrados, se não, irá renderizar os dados completos dos pacientes. Inserindo loading de carregamento enquanto ocorre o carregamento do retorno do banco de dados

--- frontend/src/components/Patients.jsx
+++ frontend/src/components/Patients.jsx
@@ -1,34 +1,57 @@
 import React, { useContext } from "react";
 import { PatientsContext } from "../context/PatientsContext";
+import Loading from "./Loading";
 
 function Patients() {
-	const { dataPatients } = useContext(PatientsContext);
+	const { dataPatients, isLoading, patientsFiltered } =
+		useContext(PatientsContext);
+
 	return (
-		<table id="patientsList">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>CPF</th>
-					<th>Status</th>
-					<th>Ações</th>
-				</tr>
-			</thead>
-			<tbody>
-				{dataPatients.map((patient) => {
-					return (
-						<tr key={patient}>
-							<td>{patient.Nome}</td>
-							<td>{patient.CPF}</td>
-							<td>{patient.Status}</td>
-							<td>
-								<a>Editar</a>
-								<a>Ver detalhes</a>
-							</td>
+		<>
+			{isLoading ? (
+				<Loading />
+			) : (
+				<table id="patientsList">
+					<thead>
+						<tr>
+							<th>Nome</th>
+							<th>CPF</th>
+							<th>Status</th>
+							<th>Ações</th>
 						</tr>
-					);
-				})}
-			</tbody>
-		</table>
+					</thead>
+					<tbody>
+						{patientsFiltered.length > 0
+							? patientsFiltered.map((patient) => {
+									return (
+										<tr key={patient}>
+											<td>{patient.nome}</td>
+											<td>{patient.CPF}</td>
+											<td>{patient.status}</td>
+											<td>
+												<a>Editar</a>
+												<a>Ver detalhes</a>
+											</td>
+										</tr>
+									);
+							  })
+							: dataPatients.map((patient) => {
+									return (
+										<tr key={patient}>
+											<td>{patient.nome}</td>
+											<td>{patient.CPF}</td>
+											<td>{patient.status}</td>
+											<td>
+												<a>Editar</a>
+												<a>Ver detalhes</a>
+											</td>
+										</tr>
+									);
+							  })}
+					</tbody>
+				</table>
+			)}
+		</>
 	);
 }
 
