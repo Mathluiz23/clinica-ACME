4025ac8e712e07dce7508d301d04371d4cdd4b68
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 17:57:06 -0300

    sparkles: feat: Importando useState e UseContext para manipular estados local e global. Implementando input, botão e lógica para a filtragem de pacientes de acordo com nome, cpf ou status

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,11 +1,40 @@
-import { React } from "react";
+/* eslint-disable array-callback-return */
+import { React, useState, useContext } from "react";
+import NavBar from "../components/NavBar";
 import Patients from "../components/Patients";
+import { PatientsContext } from "../context/PatientsContext";
 
 function MainPage() {
+	const [searchInput, setSearchInput] = useState("");
+	const { dataPatients, setPatientsFiltered } = useContext(PatientsContext);
+
+	function handleClickFilter() {
+		const filteredPatients = dataPatients.filter(
+			(patient) =>
+				patient.CPF === searchInput ||
+				patient.nome.toLowerCase() === searchInput.toLowerCase() ||
+				patient.status.toLowerCase() === searchInput.toLowerCase()
+		);
+		console.log("FILTRO AQUI", filteredPatients);
+		setPatientsFiltered(filteredPatients);
+	}
+
 	return (
 		<>
+			<NavBar />
 			<section>
-				<input type="text" value="" />
+				<label htmlFor="searchInput">
+					<input
+						type="text"
+						name="searchInput"
+						id="searchInput"
+						value={searchInput}
+						onChange={(event) => {
+							setSearchInput(event.target.value);
+						}}
+					/>
+					<button onClick={handleClickFilter}>Pesquisar</button>
+				</label>
 				<button>Cadastrar Paciente</button>
 			</section>
 			<Patients />
