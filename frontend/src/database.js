eff4d3a8dd1a4de63089b25eca4942da7ea7f968
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 10:03:29 -0300

    :sparkles: feat: Implementando mock de base de dados, que irá conter um array de objetos com a lista de paciente, com suas informações necessárias

---
+++ frontend/src/database.js
@@ -0,0 +1,37 @@
+const patientsList = [
+  {
+  id: 1,
+  Nome: "Matheus",
+  dataDeNascimento: "01/01/1990",
+  sexo: "Masculino",
+  Endereço: "Rua 1",
+  Telefone: "999999999",
+  Email: "matheus@gmail.com",
+  CPF: "03049808039",
+  Status: "Ativo"
+  },
+  {
+  id: 2,
+  Nome: "João",
+  dataDeNascimento: "01/01/1990",
+  sexo: "Masculino",
+  Endereço: "Rua 1",
+  Telefone: "999999999",
+  Email: "joao@gmail.com",
+  CPF: "1212121139",
+  Status: "Ativo"
+  },
+  {
+  id: 3,
+  Nome: "Maria",
+  dataDeNascimento: "01/01/1990",
+  sexo: "Feminino",
+  Endereço: "Rua 1",
+  Telefone: "999999999",
+  Email: "maria@gmaill.com",
+  CPF: "1212888888",
+  Status: "Ativo"
+  }
+];
+
+export default patientsList;
