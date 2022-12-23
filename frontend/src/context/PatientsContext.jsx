87790f74235d3006427978aa1cfc560cad17823d
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-23 13:40:56 -0300

    :sparkles: feat: Implementando useEffect para realizar o carregamento dos dados assim que a página é montada, utilizado setTimeout para simular o tempo de requisição fetch em um banco de dados

--- frontend/src/context/PatientsContext.jsx
+++ frontend/src/context/PatientsContext.jsx
@@ -1,10 +1,19 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import data from "../database";
 
 export const PatientsContext = createContext();
 
 function PatientsContextProvider({ children }) {
 	const [dataPatients, setDataPatients] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+		setTimeout(() => {
+			setDataPatients(data);
+		}, 2000);
+		setIsLoading(false);
+	}, []);
 
 	const context = {
 		dataPatients,
