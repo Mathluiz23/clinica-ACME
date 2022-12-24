e9c82b6a6b34db54268e3a6625b2dad31f41d658
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-24 11:51:23 -0300

    :sparkles: Implementando estrutura inicial de formulário para registro de novos pacientes utilizando biblioteca chakra-ui

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,102 @@
-import { React } from "react";
+import React from "react";
+import "../style/registerPage.css";
+
+import {
+	Flex,
+	Box,
+	Center,
+	FormControl,
+	Input,
+	FormLabel,
+	HStack,
+	RadioGroup,
+	Radio,
+	Button,
+	Select,
+} from "@chakra-ui/react";
 
 function RegisterPage() {
 	return (
 		<>
-			<h1>Registro</h1>
+			<Center className="header" as="header">
+				Cadastro de Pacientes
+			</Center>
+			<Box className="box-form">
+				<Flex className="form-container">
+					<FormControl className="form">
+						<HStack>
+							<Box className="box-form">
+								<FormLabel htmlFor="nome">
+									Nome Completo
+								</FormLabel>
+								<Input id="nome" />
+							</Box>
+							<Box className="box-form">
+								<FormLabel htmlFor="email">E-mail</FormLabel>
+								<Input isRequired id="email" type="email" />
+							</Box>
+						</HStack>
+
+						<HStack>
+							<Box className="box-form">
+								<FormLabel htmlFor="nasc">
+									Data de Nascimento
+								</FormLabel>
+								<Input id="nasc" type="date" />
+							</Box>
+							<Box className="box-form">
+								<FormLabel htmlFor="cpf">CPF</FormLabel>
+								<Input id="cpf" type="number" />
+							</Box>
+						</HStack>
+
+						<HStack>
+							<Box className="box-form">
+								<FormLabel htmlFor="endereco">
+									Endereço
+								</FormLabel>
+								<Input id="endereco" />
+							</Box>
+							<Box className="box-form">
+								<FormLabel htmlFor="cidade">Cidade</FormLabel>
+								<Input id="cidade" />
+							</Box>
+						</HStack>
+
+						<HStack>
+							<Box className="box-form">
+								<FormLabel htmlFor="cel">Celular</FormLabel>
+								<Input id="cel" type="number" />
+							</Box>
+							<Box className="box-form">
+								<FormLabel>Sexo</FormLabel>
+								<RadioGroup defaultValue="Masculino">
+									<HStack s>
+										<Radio value="Masculino">
+											Masculino
+										</Radio>
+										<Radio value="Feminino">Feminino</Radio>
+									</HStack>
+								</RadioGroup>
+							</Box>
+						</HStack>
+
+						<HStack>
+							<Box className="box-form">
+								<FormLabel htmlFor="status">Status</FormLabel>
+								<Select>
+									<option value="Ativo">Ativo</option>
+									<option value="Inativo">Inativo</option>
+								</Select>
+							</Box>
+						</HStack>
+
+						<HStack>
+							<Button>Enviar</Button>
+						</HStack>
+					</FormControl>
+				</Flex>
+			</Box>
 		</>
 	);
 }
