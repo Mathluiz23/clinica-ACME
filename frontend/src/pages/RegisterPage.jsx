1dca03c7e2acb899e7f84e1b4373b892359050c4
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-25 17:50:02 -0300

    :sparkles: feat: Implementando hooks para manipular estados e valores do formulário. Implementando lógica de buscar dados no localstorage quando a pagina é montada e setando um novo dado no localstorage com o novo paciente inserido

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { PatientsContext } from "../context/PatientsContext";
 import "../style/registerPage.css";
 
 import {
@@ -14,8 +15,60 @@ import {
 	Button,
 	Select,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 function RegisterPage() {
+	const { setDataPatints } = useContext(PatientsContext);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [cpf, setCpf] = useState("");
+	const [phone, setPhone] = useState("");
+	const [birthDate, setBirthDate] = useState("");
+	const [adress, setAddress] = useState("");
+	const [city, setCity] = useState("");
+	const [status, setStatus] = useState("Ativo");
+	const [genre, setGenre] = useState("1");
+	const [dataLocalStorage, setDataLocalStorage] = useState([]);
+	const [isValid, setIsValid] = useState(false);
+
+	useEffect(() => {
+		const dataLocalStorage = JSON.parse(localStorage.getItem("patients"));
+		setDataLocalStorage(dataLocalStorage);
+	}, []);
+
+	function handleSubmitForm(event) {
+		event.preventDefault();
+		handleValidateForm();
+
+		const newPatient = [
+			{
+				id: dataLocalStorage.length + 1,
+				nome: name,
+				email: email,
+				cpf: cpf,
+				telefone: phone,
+				dataDeNascimento: birthDate,
+				endereco: adress,
+				cidade: city,
+				status: status,
+				genero: genre,
+			},
+		];
+
+		if (isValid) {
+			const newData = [...dataLocalStorage, ...newPatient];
+			setDataLocalStorage(newData);
+			localStorage.setItem("patients", JSON.stringify(newData));
+			setDataPatints(newData);
+		}
+	}
+
+	function handleValidateForm() {
+		if (name && email && cpf && phone && birthDate && city && status) {
+			setIsValid(true);
+		}
+	}
+
 	return (
 		<>
 			<Center className="header" as="header">
@@ -26,14 +79,28 @@ function RegisterPage() {
 					<FormControl className="form">
 						<HStack>
 							<Box className="box-form">
-								<FormLabel htmlFor="nome">
-									Nome Completo
-								</FormLabel>
-								<Input id="nome" />
+								<FormLabel htmlFor="nome">Nome</FormLabel>
+								<Input
+									id="nome"
+									placeholder="Informe o nome completo"
+									onChange={(event) => {
+										setName(event.target.value);
+									}}
+									value={name}
+								/>
 							</Box>
 							<Box className="box-form">
 								<FormLabel htmlFor="email">E-mail</FormLabel>
-								<Input isRequired id="email" type="email" />
+								<Input
+									isRequired
+									id="email"
+									type="email"
+									placeholder="exemplo@interprocess.com"
+									onChange={(event) => {
+										setEmail(event.target.value);
+									}}
+									value={email}
+								/>
 							</Box>
 						</HStack>
 
@@ -42,11 +109,26 @@ function RegisterPage() {
 								<FormLabel htmlFor="nasc">
 									Data de Nascimento
 								</FormLabel>
-								<Input id="nasc" type="date" />
+								<Input
+									id="nasc"
+									type="date"
+									onChange={(event) => {
+										setBirthDate(event.target.value);
+									}}
+									value={birthDate}
+								/>
 							</Box>
 							<Box className="box-form">
 								<FormLabel htmlFor="cpf">CPF</FormLabel>
-								<Input id="cpf" type="number" />
+								<Input
+									id="cpf"
+									type="number"
+									placeholder="000-000.000-00"
+									onChange={(event) => {
+										setCpf(event.target.value);
+									}}
+									value={cpf}
+								/>
 							</Box>
 						</HStack>
 
@@ -55,27 +137,50 @@ function RegisterPage() {
 								<FormLabel htmlFor="endereco">
 									Endereço
 								</FormLabel>
-								<Input id="endereco" />
+								<Input
+									id="endereco"
+									placeholder="Rua Brasil 1"
+									onChange={(event) => {
+										setAddress(event.target.value);
+									}}
+									value={adress}
+								/>
 							</Box>
 							<Box className="box-form">
 								<FormLabel htmlFor="cidade">Cidade</FormLabel>
-								<Input id="cidade" />
+								<Input
+									id="cidade"
+									placeholder="Porto Alegre"
+									onChange={(event) => {
+										setCity(event.target.value);
+									}}
+									value={city}
+								/>
 							</Box>
 						</HStack>
 
 						<HStack>
 							<Box className="box-form">
 								<FormLabel htmlFor="cel">Celular</FormLabel>
-								<Input id="cel" type="number" />
+								<Input
+									id="cel"
+									type="number"
+									placeholder="(51) 99999-9999"
+									onChange={(event) => {
+										setPhone(event.target.value);
+									}}
+									value={phone}
+								/>
 							</Box>
 							<Box className="box-form">
 								<FormLabel>Sexo</FormLabel>
-								<RadioGroup defaultValue="Masculino">
-									<HStack s>
+								<RadioGroup onChange={setGenre} value={genre}>
+									<HStack>
 										<Radio value="Masculino">
 											Masculino
 										</Radio>
 										<Radio value="Feminino">Feminino</Radio>
+										<Radio value="Outro">Outro</Radio>
 									</HStack>
 								</RadioGroup>
 							</Box>
@@ -84,7 +189,12 @@ function RegisterPage() {
 						<HStack>
 							<Box className="box-form">
 								<FormLabel htmlFor="status">Status</FormLabel>
-								<Select>
+								<Select
+									variant="flushed"
+									onChange={(event) => {
+										setStatus(event.target.value);
+									}}
+								>
 									<option value="Ativo">Ativo</option>
 									<option value="Inativo">Inativo</option>
 								</Select>
@@ -92,7 +202,15 @@ function RegisterPage() {
 						</HStack>
 
 						<HStack>
-							<Button>Enviar</Button>
+							<Button
+								className="submit-button"
+								type="button"
+								colorScheme="rgb(18, 7, 88)"
+								// isDisabled={isValid}
+								onClick={handleSubmitForm}
+							>
+								Enviar
+							</Button>
 						</HStack>
 					</FormControl>
 				</Flex>
