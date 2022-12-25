9cfe2b2d8e75bafa8cc7cf8eb16fb419bf405edc
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-25 13:06:30 -0300

    :recycle: refactor: Modificando tags do html por tags da biblioteca chakra. Importando useNavigate para executar a mudança de rota no botão de registrar paciente

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,12 +1,16 @@
-/* eslint-disable array-callback-return */
 import { React, useState, useContext } from "react";
 import NavBar from "../components/NavBar";
 import Patients from "../components/Patients";
 import { PatientsContext } from "../context/PatientsContext";
+import { useNavigate } from "react-router-dom";
+import { Button, FormLabel, Box, Input } from "@chakra-ui/react";
+import "../style/mainPage.css";
+import "../style/shared.css";
 
 function MainPage() {
 	const [searchInput, setSearchInput] = useState("");
 	const { dataPatients, setPatientsFiltered } = useContext(PatientsContext);
+	const navigate = useNavigate();
 
 	function handleClickFilter() {
 		const filteredPatients = dataPatients.filter(
@@ -17,27 +21,49 @@ function MainPage() {
 					.includes(searchInput.toLowerCase()) ||
 				patient.status.toLowerCase() === searchInput.toLowerCase()
 		);
-		console.log("FILTRO AQUI", filteredPatients);
 		setPatientsFiltered(filteredPatients);
+	}
+
+	function registerPatient() {
+		navigate("/register");
 	}
 
 	return (
 		<>
 			<NavBar />
-			<section>
-				<label htmlFor="searchInput">
-					<input
-						type="text"
-						name="searchInput"
-						id="searchInput"
-						value={searchInput}
-						onChange={(event) => {
-							setSearchInput(event.target.value);
-						}}
-					/>
-					<button onClick={handleClickFilter}>Pesquisar</button>
-				</label>
-				<button>Cadastrar Paciente</button>
+
+			<section className="search-container">
+				<Box className="box-form search-input">
+					<FormLabel htmlFor="searchInput">
+						<Input
+							type="text"
+							name="searchInput"
+							id="searchInput"
+							placeholder="Pesquise o paciente por nome, CPF ou status"
+							value={searchInput}
+							onChange={(event) => {
+								setSearchInput(event.target.value);
+							}}
+						/>
+					</FormLabel>
+				</Box>
+
+				<Button
+					className="search-button"
+					type="button"
+					colorScheme="rgb(18, 7, 88)"
+					onClick={handleClickFilter}
+				>
+					Pesquisar Paciente
+				</Button>
+				<Button
+					className="register-button"
+					type="button"
+					colorScheme="rgb(18, 7, 88)"
+					onClick={registerPatient}
+				>
+					Cadastrar Paciente
+				</Button>
 			</section>
 			<Patients />
 		</>
