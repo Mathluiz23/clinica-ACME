64ee871f3d19fddca765c9402640039da88ae8ed
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-26 19:58:54 -0300

    :constructor: progress: Implementando componente NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,24 @@
 import { React } from "react";
+import { Link } from "react-router-dom";
+import "../style/shared.css";
 
 function NavBar() {
 	return (
-		<nav>
-			<header>Clínica ACME</header>
-			<ul>
-				<a>
-					<li>Pacientes</li>
-				</a>
-			</ul>
-		</nav>
+		<>
+			<nav className="side-bar">
+				<ul className="nav-links">
+					<a className="nav-item" href="/">
+						<li>Pacientes</li>
+					</a>
+					<a className="nav-item" href="/">
+						<li>Empresa Parceira</li>
+					</a>
+					<a className="nav-item" href="/">
+						<li>Localização</li>
+					</a>
+				</ul>
+			</nav>
+		</>
 	);
 }
 
