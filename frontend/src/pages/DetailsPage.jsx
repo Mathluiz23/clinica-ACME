618bb93b27501b19de89ac29131febb1ed6e200a
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-26 20:00:35 -0300

    :sparkles: feat: Implementando lógica de editar pacientes

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -13,11 +13,14 @@ import {
 	RadioGroup,
 	Radio,
 	Select,
+	Button,
 } from "@chakra-ui/react";
 import Loading from "../components/Loading";
+import Swal from "sweetalert2";
+import NavBar from "../components/NavBar";
 
 function DetailsPage() {
-	const { dataPatients, isLoading, setIsLoading, setDataPatients } =
+	const { dataPatients, isLoading, setDataPatients } =
 		useContext(PatientsContext);
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
@@ -29,6 +32,7 @@ function DetailsPage() {
 	const [status, setStatus] = useState("Ativo");
 	const [genre, setGenre] = useState("Masculino");
 	const [patientDetailsById, setPatientDetailsById] = useState([]);
+	const [isEdit, setIsEdit] = useState(false);
 
 	const { pathname } = useLocation();
 	const patientId = pathname.split("/")[2];
@@ -45,7 +49,7 @@ function DetailsPage() {
 			setCpf(patientById.cpf);
 			setPhone(patientById.telefone);
 			setBirthDate(patientById.dataDeNascimento);
-			setAddress(patientById.endereco);
+			setAddress(patientById.endereço);
 			setCity(patientById.cidade);
 			setStatus(patientById.status);
 			setGenre(patientById.genero);
@@ -58,9 +62,55 @@ function DetailsPage() {
 		}
 	}, [dataPatients]);
 
+	function handleEditPatient() {
+		setIsEdit(true);
+	}
+
+	function handleUpdatePatient() {
+		if (name === "" || email === "" || cpf === "" || phone === "") {
+			alert("Preencha todos os campos!");
+		} else {
+			const [patientById] = dataPatients.filter(
+				(patient) => patient.id == patientId
+			);
+
+			const index = dataPatients.indexOf(patientById);
+
+			const newPatient = {
+				id: patientById.id,
+				nome: name,
+				email: email,
+				cpf: cpf,
+				telefone: phone,
+				dataDeNascimento: birthDate,
+				endereço: adress,
+				cidade: city,
+				status: status,
+				genero: genre,
+			};
+
+			const newDataPatients = [...dataPatients];
+			newDataPatients[index] = newPatient;
+
+			setDataPatients(newDataPatients);
+			localStorage.setItem("patients", JSON.stringify(newDataPatients));
+		}
+
+		setIsEdit(false);
+
+		Swal.fire({
+			position: "center",
+			icon: "success",
+			title: "Paciente atualizado com sucesso!",
+			showConfirmButton: false,
+			timer: 1500,
+		});
+	}
+
 	return (
 		<>
 			<h1 className="header">Detalhes do Paciente</h1>
+			<NavBar />
 
 			{isLoading ? (
 				<Loading />
@@ -77,6 +127,8 @@ function DetailsPage() {
 											setName(event.target.value);
 										}}
 										value={name}
+										disabled={isEdit ? false : true}
+										isRequired={true}
 									/>
 								</Box>
 								<Box className="box-form">
@@ -84,13 +136,14 @@ function DetailsPage() {
 										E-mail
 									</FormLabel>
 									<Input
-										isRequired
+										isRequired={true}
 										id="email"
 										type="email"
 										onChange={(event) => {
 											setEmail(event.target.value);
 										}}
 										value={email}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 							</HStack>
@@ -102,11 +155,12 @@ function DetailsPage() {
 									</FormLabel>
 									<Input
 										id="nasc"
-										type="date"
+										type="text"
 										onChange={(event) => {
 											setBirthDate(event.target.value);
 										}}
 										value={birthDate}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 								<Box className="box-form">
@@ -114,11 +168,11 @@ function DetailsPage() {
 									<Input
 										id="cpf"
 										type="number"
-										placeholder="000-000.000-00"
 										onChange={(event) => {
 											setCpf(event.target.value);
 										}}
 										value={cpf}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 							</HStack>
@@ -130,11 +184,11 @@ function DetailsPage() {
 									</FormLabel>
 									<Input
 										id="endereco"
-										placeholder="Rua Brasil 1"
 										onChange={(event) => {
 											setAddress(event.target.value);
 										}}
 										value={adress}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 								<Box className="box-form">
@@ -143,11 +197,11 @@ function DetailsPage() {
 									</FormLabel>
 									<Input
 										id="cidade"
-										placeholder="Porto Alegre"
 										onChange={(event) => {
 											setCity(event.target.value);
 										}}
 										value={city}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 							</HStack>
@@ -158,11 +212,11 @@ function DetailsPage() {
 									<Input
 										id="cel"
 										type="number"
-										placeholder="(51) 99999-9999"
 										onChange={(event) => {
 											setPhone(event.target.value);
 										}}
 										value={phone}
+										disabled={isEdit ? false : true}
 									/>
 								</Box>
 								<Box className="box-form">
@@ -170,6 +224,7 @@ function DetailsPage() {
 									<RadioGroup
 										onChange={setGenre}
 										value={genre}
+										disabled={isEdit ? false : true}
 									>
 										<HStack>
 											<Radio value="Masculino">
@@ -194,11 +249,32 @@ function DetailsPage() {
 										onChange={(event) => {
 											setStatus(event.target.value);
 										}}
+										value={status}
+										disabled={isEdit ? false : true}
 									>
 										<option value="Ativo">Ativo</option>
 										<option value="Inativo">Inativo</option>
 									</Select>
 								</Box>
+							</HStack>
+							<HStack>
+								<Button
+									className="register-button"
+									type="button"
+									onClick={handleEditPatient}
+									colorScheme="blue"
+								>
+									Editar
+								</Button>
+
+								<Button
+									className="register-button"
+									type="button"
+									onClick={handleUpdatePatient}
+									colorScheme="blue"
+								>
+									Salvar
+								</Button>
 							</HStack>
 						</FormControl>
 					</Flex>
