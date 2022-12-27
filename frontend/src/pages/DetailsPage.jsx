f0b1a0f8f5c0493532585e0a5fdcae7ce4d2b6d3
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-12-27 17:15:40 -0300

    :recycle: feat: Ajustando detailsPage após componentização do formulário. Enviando parâmetros específicos utilizados na página de detalhes via props, através do component PatientForm

--- frontend/src/pages/DetailsPage.jsx
+++ frontend/src/pages/DetailsPage.jsx
@@ -2,29 +2,17 @@ import { React, useState, useContext, useEffect } from "react";
 import "../style/shared.css";
 import { PatientsContext } from "../context/PatientsContext";
 import { useLocation } from "react-router-dom";
-import FormErrorMessage from "../components/ErrorMessage";
 
-import {
-	Box,
-	Flex,
-	FormControl,
-	FormLabel,
-	Input,
-	HStack,
-	RadioGroup,
-	Radio,
-	Select,
-	Button,
-} from "@chakra-ui/react";
 import Loading from "../components/Loading";
 import Swal from "sweetalert2";
 import NavBar from "../components/NavBar";
+import PatientForm from "../components/Form/PatientForm";
+
+const NON_REQUIRED_FIELDS = ["address"];
 
 function DetailsPage() {
 	const { dataPatients, isLoading, setDataPatients } =
 		useContext(PatientsContext);
-
-	const [isEdit, setIsEdit] = useState(false);
 
 	const [isUserValid, setIsUserValid] = useState(null);
 	const [register, setRegister] = useState({});
@@ -32,13 +20,8 @@ function DetailsPage() {
 
 	const { pathname } = useLocation();
 	const patientId = pathname.split("/")[2];
-	// console.log("ID DA URL", patientId);
 
 	useEffect(() => {
-		const [patientById] = dataPatients.filter(
-			(patient) => patient.id === +patientId
-		);
-
 		if (dataPatients.length) {
 			const currentPatient = dataPatients.find((patient) => {
 				return patient.id === +patientId;
@@ -53,9 +36,9 @@ function DetailsPage() {
 		}
 	}, [patientId, dataPatients]);
 
-	function handleEditPatient() {
-		setIsEdit(true);
-	}
+	// function handleEditPatient() {
+	// 	setIsEdit(true);
+	// }
 
 	const handleOnChangeGenre = (value) => {
 		// console.log(value);
@@ -78,7 +61,9 @@ function DetailsPage() {
 
 		//Verificando se algum dos inputs está vazio
 		for (const key in register) {
-			console.log(key, register, "KEEY E REGISTER AQUIIIII");
+			if (NON_REQUIRED_FIELDS.includes(key)) {
+				continue;
+			}
 			if (register[key] === "") {
 				result = false;
 				validationMessages[key] = "Campo Obrigatório";
@@ -111,7 +96,6 @@ function DetailsPage() {
 				confirmButtonColor: "#2D9CDB",
 			});
 		}
-		setIsEdit(false);
 	}
 
 	return (
@@ -124,182 +108,14 @@ function DetailsPage() {
 			{isLoading ? (
 				<Loading />
 			) : (
-				<Box className="box-form">
-					<Flex className="form-container">
-						<FormControl className="form">
-							<HStack>
-								<Box className="box-form">
-									<FormLabel htmlFor="nome">Nome</FormLabel>
-									<Input
-										name="name"
-										onChange={handleOnChange}
-										value={register.name}
-										disabled={isEdit ? false : true}
-										isRequired={true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-								<Box className="box-form">
-									<FormLabel htmlFor="email">
-										E-mail
-									</FormLabel>
-									<Input
-										isRequired={true}
-										name="email"
-										type="email"
-										onChange={handleOnChange}
-										value={register.email}
-										disabled={isEdit ? false : true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-							</HStack>
-
-							<HStack>
-								<Box className="box-form">
-									<FormLabel htmlFor="nasc">
-										Data de Nascimento
-									</FormLabel>
-									<Input
-										id="nasc"
-										type="text"
-										onChange={handleOnChange}
-										value={register.birthDate}
-										disabled={isEdit ? false : true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-								<Box className="box-form">
-									<FormLabel htmlFor="cpf">CPF</FormLabel>
-									<Input
-										name="cpf"
-										type="number"
-										onChange={handleOnChange}
-										value={register.cpf}
-										disabled={true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-							</HStack>
-
-							<HStack>
-								<Box className="box-form">
-									<FormLabel htmlFor="address">
-										Endereço
-									</FormLabel>
-									<Input
-										id="address"
-										onChange={handleOnChange}
-										value={register.adress}
-										disabled={isEdit ? false : true}
-									/>
-								</Box>
-								<Box className="box-form">
-									<FormLabel htmlFor="city">Cidade</FormLabel>
-									<Input
-										name="city"
-										onChange={handleOnChange}
-										value={register.city}
-										disabled={isEdit ? false : true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-							</HStack>
-
-							<HStack>
-								<Box className="box-form">
-									<FormLabel htmlFor="phone">
-										Celular
-									</FormLabel>
-									<Input
-										name="phone"
-										type="number"
-										onChange={handleOnChange}
-										value={register.phone}
-										disabled={isEdit ? false : true}
-									/>
-									<FormErrorMessage
-										errors={formValidation}
-										fieldName="name"
-									/>
-								</Box>
-								<Box className="box-form">
-									<FormLabel>Sexo</FormLabel>
-									<RadioGroup
-										name="genre"
-										onChange={handleOnChangeGenre}
-										value={register.genre}
-										disabled={isEdit ? false : true}
-									>
-										<HStack>
-											<Radio value="Masculino">
-												Masculino
-											</Radio>
-											<Radio value="Feminino">
-												Feminino
-											</Radio>
-											<Radio value="Outro">Outro</Radio>
-										</HStack>
-									</RadioGroup>
-								</Box>
-							</HStack>
-
-							<HStack>
-								<Box className="box-form">
-									<FormLabel htmlFor="status">
-										Status
-									</FormLabel>
-									<Select
-										name="status"
-										variant="flushed"
-										onChange={handleOnChange}
-										value={register.status}
-										disabled={isEdit ? false : true}
-									>
-										<option value="Ativo">Ativo</option>
-										<option value="Inativo">Inativo</option>
-									</Select>
-								</Box>
-							</HStack>
-							<HStack>
-								<Button
-									className="register-button"
-									type="button"
-									onClick={handleEditPatient}
-									colorScheme="blue"
-									disabled={isEdit}
-								>
-									Editar
-								</Button>
-
-								<Button
-									className="register-button"
-									type="button"
-									onClick={handleUpdatePatient}
-									colorScheme="blue"
-									disabled={!isEdit}
-								>
-									Salvar
-								</Button>
-							</HStack>
-						</FormControl>
-					</Flex>
-				</Box>
+				<PatientForm
+					form={register}
+					handleOnChange={handleOnChange}
+					formValidation={formValidation}
+					handleOnChangeGenre={handleOnChangeGenre}
+					handleSubmitForm={handleUpdatePatient}
+					isEditForm={true}
+				/>
 			)}
 		</>
 	);
