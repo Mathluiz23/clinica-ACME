f9ca15eb3244037f53c69defa618cde6a4594d1b
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-20 15:02:29 -0300

    :recycle: feat: Importando pagina de login para renderização na rota home (/)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import MainPage from "./pages/mainPage/MainPage";
 import DetailsPage from "./pages/detailsPage/DetailsPage";
 import RegisterPage from "./pages/registerPage/RegisterPage";
 import "./style/App.css";
+import LoginPage from "./pages/login/LoginPage";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
-				<Route exact path="/" element={<MainPage />} />
+				<Route exact path="/" element={<LoginPage />} />
+				<Route exact path="/patient" element={<MainPage />} />
 				<Route exact path="/details/:id" element={<DetailsPage />} />
 				<Route exact path="/register" element={<RegisterPage />} />
 			</Routes>
